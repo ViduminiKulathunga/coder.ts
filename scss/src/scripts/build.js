13aa97a3c33869877da8f375b9bcf40f42ef2332
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-06-02 23:03:02 +0200

    modern-normalize

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -2,13 +2,18 @@ const fs = require("node:fs");
 const path = require("node:path");
 const sass = require("sass");
 
-const inputPath = path.resolve("src/global.scss");
-const outputPath = path.resolve("dist/global.css");
+const inputScss = path.resolve("src/global.scss");
+const outputCss = path.resolve("dist/global.css");
+const normalizeCssSrc = path.resolve(
+	"node_modules/modern-normalize/modern-normalize.css",
+);
+const normalizeCssDest = path.resolve("dist/modern-normalize.css");
 
-const result = sass.compile(inputPath, {
+const result = sass.compile(inputScss, {
 	style: "expanded",
 	loadPaths: [path.resolve("src"), path.resolve("node_modules")],
 });
 
-fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-fs.writeFileSync(outputPath, result.css);
+fs.mkdirSync(path.dirname(outputCss), { recursive: true });
+fs.writeFileSync(outputCss, result.css);
+fs.copyFileSync(normalizeCssSrc, normalizeCssDest);
