10f553cce19c52b70f5c13ebd200786889f6b6b0
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-06-03 14:52:47 +0200

    scss to css

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -2,25 +2,44 @@ const fs = require("node:fs");
 const path = require("node:path");
 const sass = require("sass");
 
-try {
-	const inputPath = path.resolve("src/global.scss");
-	const outputPath = path.resolve("src/lib/global.css");
+const normalizeCSSPath = require.resolve(
+	"modern-normalize/modern-normalize.css",
+);
+const normalizeCSS = fs.readFileSync(normalizeCSSPath, "utf8");
 
-	const normalizeCSSPath = require.resolve(
-		"modern-normalize/modern-normalize.css",
-	);
-	const normalizeCSS = fs.readFileSync(normalizeCSSPath, "utf8");
+const getComponents = () => {
+	let allComponents = [];
+	const types = ["atoms", "molecules", "organisom"];
 
-	const sassResult = sass.compile(inputPath, {
+	types.forEach((type) => {
+		const allFiles = fs.readdirSync(`src/${type}`).map((file) => ({
+			input: `src/${type}/${file}`,
+			output: `src/lib/${file.slice(0, -4) + "css"}`,
+		}));
+
+		allComponents = [...allComponents, ...allFiles];
+	});
+
+	return allComponents;
+};
+
+const compile = (pathFile, fileName) => {
+	const sassResult = sass.compile(pathFile, {
 		style: "expanded",
 		loadPaths: [path.resolve("src"), path.resolve("node_modules")],
 	});
 
 	const finalCSS = `${normalizeCSS}\n\n${sassResult.css.toString()}`;
 
-	fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-	fs.writeFileSync(outputPath, finalCSS);
-} catch (error) {
-	console.error("Build failed:", error);
-	process.exit(1);
-}
+	fs.mkdirSync(path.dirname(fileName), { recursive: true });
+	fs.writeFileSync(fileName, finalCSS);
+};
+
+const pathFile = path.resolve("src/global.scss");
+const fileName = path.resolve("src/lib/global.css");
+
+compile(pathFile, fileName);
+
+getComponents().forEach((component) => {
+	compile(component.input, component.output);
+});
