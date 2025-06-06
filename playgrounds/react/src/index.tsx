e628cee593873e12e148f80f030b3e8a6f337d6f
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-06-07 00:47:01 +0200

    Developing Color Component

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-import { Button } from "@coder.ts/react";
+import { Button, Color } from "@coder.ts/react";
 import "@coder.ts/scss/lib/Button.css"
 
 const rootElement = document.querySelector("#root");
 const root = ReactDOM.createRoot(rootElement!);
 
 root.render(
-    <Button label="Submit" />
+    <>
+        <Button label="Submit" />
+        <Color hexCode="#012" width="1rem" height="1rem" />
+    </>
 );
